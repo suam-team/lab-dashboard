30d7d21a1a51935cbc69514ce0215d335049cfae
Pongsakorn Sommalai <p.sommalai@sth.sh>
2020-11-23 13:42:59 +0700

    Add PREFERRED_URL_SCHEME environment

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or 'sqlite:///database.db'
 app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
-app.config['GITHUB_REDIRECT_URL'] = os.environ.get("GITHUB_REDIRECT_URL")
+app.config['PREFERRED_URL_SCHEME'] = os.environ.get("PREFERRED_URL_SCHEME")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
 app.config["GITHUB_LAB_USER"] = os.environ.get("GITHUB_LAB_USER")
 app.config["GOOGLE_GLOBAL_SITE_TAG"] = os.environ.get("GOOGLE_GLOBAL_SITE_TAG")
+app.config['PREFERRED_URL_SCHEME'] = os.environ.get("PREFERRED_URL_SCHEME")
 
 admins = os.environ.get("DASHBOARD_ADMIN") or ""
 app.config["DASHBOARD_ADMIN"] = []
