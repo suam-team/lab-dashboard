250506f07143dd538ab432ee41a74e08734d53ed
Pongsakorn Sommalai <p.sommalai@sth.sh>
2020-11-23 13:25:21 +0700

    Add github redirect url environment

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or 'sqlite:///database.db'
 app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
+app.config['GITHUB_REDIRECT_URL'] = os.environ.get("GITHUB_REDIRECT_URL")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
 app.config["GITHUB_LAB_USER"] = os.environ.get("GITHUB_LAB_USER")
 app.config["GOOGLE_GLOBAL_SITE_TAG"] = os.environ.get("GOOGLE_GLOBAL_SITE_TAG")
--- dashboard/routes.py
+++ dashboard/routes.py
@@ -12,7 +12,7 @@ from . util import decrypt_flag, encrypt_flag
 from dashboard import app
 
 github_bp = make_github_blueprint()
-app.register_blueprint(github_bp, url_prefix="/login")
+app.register_blueprint(github_bp, url_prefix="/login", redirect_url=app.config.get('GITHUB_REDIRECT_URI'))
 
 github_sdk = Github()
 
